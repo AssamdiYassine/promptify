0f974699b5f0e81228c0766d8e38f170ea784cb7
ASSAMDI YASSINE <assamdiyassine1@gmail.com>
2023-05-28 20:53:28 +0100

    test url img in deploy

--- src/component/NavBar2.tsx
+++ src/component/NavBar2.tsx
@@ -39,7 +39,7 @@ export default function NavBar() {
                                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                     <div className="flex flex-shrink-0 items-center">
                                         <SVG
-                                            src="../../public/logo.svg"
+                                            src="https://promptify-3n1t.vercel.app/public/logo.svg"
                                             width={"100px"}
                                         />
                                     </div>
